bc27992253ff0c38dfbf4d310c77cd3f7b11d15c
Prashant Singh <52065013+PrashantSinghT99@users.noreply.github.com>
2023-07-13 15:51:41 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-// const DB = "mongodb://awesomeharrysingh:awesomeharrysingh@ac-rcvyzk0-shard-00-00.3jmxzok.mongodb.net:27017,ac-rcvyzk0-shard-00-01.3jmxzok.mongodb.net:27017,ac-rcvyzk0-shard-00-02.3jmxzok.mongodb.net:27017/mernApp?ssl=true&replicaSet=atlas-7e55en-shard-0&authSource=admin&retryWrites=true&w=majority"
+
 const DB = process.env.DATABASE;
 // mongoose.connect(DB, {
 //     useUnifiedTopology: true,
